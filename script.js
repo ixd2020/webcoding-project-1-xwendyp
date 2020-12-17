236607ae5c8dabd3dea29dfa3f1ee6ee5b6f14ff
xwendyp <73972339+xwendyp@users.noreply.github.com>
2020-12-17 21:53:53 +0800

    Updated Version

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function mobilemenu() {
     if (x.className === "topnav") {
         x.className = x.className + " responsive";
     } else {
-        x.className = "topnav";
+        x.className = "topnav"
     }
 }
 
@@ -19,32 +19,11 @@ function mobilemenu() {
 document.getElementById("subscribe-button").addEventListener("click", subscribe);
 
 function subscribe() {
-    alert("Success. You have subscribed.");
+    alert("You have subscribed.");
 
 }
-// Slideshow
-var slideIndex = 0;
-showSlides();
 
-function showSlides() {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("dot");
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    // previously have ;
-    slideIndex++
-    if (slideIndex > slides.length) {
-        slideIndex = 1;
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace("active ", "");
-    }
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
-    setTimeout(showSlides, 5000);
-}
+
 //accordion
 var acc = document.getElementsByClassName("accordion");
 var i;
